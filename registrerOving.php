98ed46e5bf1e294e910afaaed518731512a47238
tiriksan <kristian.aabrekk@gmail.com>
2014-04-29 10:54:03 +0200

    registrering

--- registrerOving.php
+++ registrerOving.php
@@ -4,17 +4,48 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+include './service.incl.php';
+sjekkOmAdmin();
+?>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" href="stilark.css" />
+        <title>Registrer øving</title>
     </head>
     <body>
-        <?php 
-        adminMeny();
-        ?>
         <?php
-        // put your code here
+        adminMeny();
+
+        if (isset($_POST['registrer'])) {
+            echo leggTilOving();
+        }
         ?>
+        <div id="wrapper">
+            <h1>Registrering</h1>
+            <form method="POST">
+                <table>
+                    <thead>
+                    <th colspan="2">Registrer ny øving</th>
+                    </thead>
+                    <tr>
+                        <td><label for="ovingsNavn">Øvings navn:</label></td>
+                        <td><input name="ovingsNavn" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="innleveringsFrist">Innleveringsfrist:</label></td>
+                        <td><input type="text" name="innleveringsFrist"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="obligatorisk">Obligatorisk</label></td>
+                        <td><input type="checkbox" name="obligatorisk"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="submit" value="Legg til øving" name="leggTilOving"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     </body>
 </html>
