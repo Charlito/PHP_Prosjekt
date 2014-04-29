7ec244644e04172b0a3dba7a191098bf78d9a393
tiriksan <kristian.aabrekk@gmail.com>
2014-04-29 17:25:08 +0200

    visOversikt
    kadfsdfsafsadkfdwa

--- visOversikt.php
+++ visOversikt.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <?php
 include './service.incl.php';
 sjekkOmAdmin();
@@ -17,32 +12,39 @@ sjekkOmAdmin();
     <body>
         <?php
         adminMeny();
+
+        
+        if (isset($_POST))
+            //TODO slettOving(key($_POST))
+            echo key($_POST);
         ?>
         <div id="wrapper">
             <h1>Vis oversikt</h1>
-            <table>
-                <thead>
-                <th>Øvinger</th>
-                <th>Antall innleveringer</th>
-                <th>Mangler tilbakemelding</th>
-                <th>Antall godkjente</th>
-                <th>Slett øving</th>
-                </thead>
-                <?php
-                $ovinger = getOvinger();
-                $ovingInfo = getOvingerInfo();
-                for ($i = 0; $i < count($ovinger); $i++) {
-                    echo "<tr>"
-                    . "<td><a href='visOvingOversikt.php?ovingsID=" . $ovinger[$i]['ovingsID'] ."'>" . $ovinger[$i]['navn'] . "</a></td>"
-                    . "<td>" . $ovingInfo[$i]['antInnleveringer'] . "</td>"
-                    . "<td>" . $ovingInfo[$i]['manglendeTilbakemelding'] . "</td>"
-                    . "<td>" . $ovingInfo[$i]['antGodkjente'] . "</td>"
-                    . "<td><input type='submit' name='" . $ovinger[$i]['ovingsID'] . "'></td>"
-                    . "</tr>";
-                }
-                // put your code here
-                ?>
-            </table>
+            <form method="POST">
+                <table>
+                    <thead>
+                    <th>Øvinger</th>
+                    <th>Antall innleveringer</th>
+                    <th>Mangler tilbakemelding</th>
+                    <th>Antall godkjente</th>
+                    <th>Slett øving</th>
+                    </thead>
+                    <?php
+                    $ovinger = getOvinger();
+                    $ovingInfo = getOvingerInfo();
+                    for ($i = 0; $i < count($ovinger); $i++) {
+                        echo "<tr>"
+                        . "<td><a href='visOvingOversikt.php?ovingsID=" . $ovinger[$i]['ovingsID'] . "'>" . $ovinger[$i]['navn'] . "</a></td>"
+                        . "<td>" . $ovingInfo[$i]['antInnleveringer'] . "</td>"
+                        . "<td>" . $ovingInfo[$i]['manglendeTilbakemelding'] . "</td>"
+                        . "<td>" . $ovingInfo[$i]['antGodkjente'] . "</td>"
+                        . "<td><input type='submit' name='" . $ovinger[$i]['ovingsID'] . "' value='Slett'></td>"
+                        . "</tr>";
+                    }
+                    // put your code here
+                    ?>
+                </table>
+            </form>
         </div>
     </body>
 </html>
